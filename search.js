3d29f18a707d0e9c1bde5fca98e66ba4e8f894df
Maglaque, Adrian <158177727+Maglaqueworkshop@users.noreply.github.com>
2025-01-14 06:59:42 +0800

    Add files via upload

--- search.js
+++ search.js
@@ -1,25 +1,80 @@
-import {
-    Accounts,
-    Volibear,
-    Malphite,
-    Nocturn,
-    QuestionMark
-} from './Champion.js';
+var kayle = `
+<p>Teemo[Flash, Ignite]</p><hr>
+<p>Malphite[Tp, Ignite]</p><hr>
+`;
 
+
+var darius  = `
+<p>Malphite[Tp, Ignite]</p><hr>
+`;
+
+var teemo  = `
+<p>Malphite[Tp, Ignite]</p><hr>
+<p>Vladimir[Tp, Flash]</p><hr>
+<p>Gang Plank[Tp, Flash]</p><hr>
+
+
+`;
+
+
+var malzahar  = `
+<p>galio[Tp, flash]</p><hr>
+`;
+
+
+
+var sion = `
+<p>Teemo[Flash, Ignite]</p><hr>
+`;
+
+var ahri = `
+<p>galio[Tp, flash]</p><hr>
+
+`;
+
+
+var maokai = `
+<p>galio[Tp, flash]</p><hr>
+`;
+
+var volibear = `
+<p>Naafiri[Tp, flash]</p><hr>
+`;
 
 
 
 var pages = [
-    {title: "Accounts", content: Accounts},
-    {title: "Volibear", content: Volibear},
-    {title: "Malphite", content: Malphite},
-    {title: "Nocturn", content: Nocturn},
-    {title: "??", content: QuestionMark},
+    {title: "Kayle", content: kayle},
+    {title: "Teemo", content: teemo},
+    {title: "Darius", content: darius},
+    {title: "Sion", content: sion},
+    {title: "Malzahar", content: malzahar},
+    {title: "Ahri", content: ahri},
+    {title: "Maokai", content: maokai},
+    {title: "Volibear", content: volibear},
+
+
+
+
+
+
+
 
 ]
 
 
 
+
+
+var tests = [
+    {name: 'ws'},
+    {name: 's3'},
+]
+
+
+for (var i = 0; i < tests.length; i++) {
+    console.log(i+1);
+}
 
 
 
@@ -39,7 +94,11 @@ var linksContainer = document.getElementById("links");
 var suggestions = document.getElementById("searchSuggestions");
 var logo = document.getElementById("logo");
 var returnDiv = document.getElementById("return_div");
-searchBar.setAttribute("placeholder", "Search Hero to Counter")
+searchBar.setAttribute("placeholder", "Search Hero to Counter");
+
+
+
+
 
 
 
@@ -54,7 +113,7 @@ searchBar.addEventListener("keyup", (e) => {
     var searchValue = searchBar.value.toLowerCase();
     suggestions.innerHTML = "";
     var pagesTitles = pages.filter((e) => {
-        return e.title.toLowerCase().includes(searchValue) || e.content.toLowerCase().includes(searchValue);
+        return e.title.toLowerCase().includes(searchValue);
     })
     pagesTitles.forEach((i)=>{
         var a = document.createElement("a");
@@ -64,12 +123,23 @@ searchBar.addEventListener("keyup", (e) => {
             suggestions.innerHTML = "";
             links.preventDefault();
             overlay.setAttribute("id", "shadow_wrapperOn");
+            searchBar.value = "";
+
             document.title = `Lucky | ${i.title}`;
             popupTitle.innerHTML = i.title;
             var newDiv = document.createElement("div");
             newDiv.setAttribute("id", "iContent");
             newDiv.innerHTML = i.content;
             popUpDiv.appendChild(newDiv)
+            overlay.addEventListener("click", (event)=> {
+                if (event.target === overlay) {
+                    overlay.setAttribute("id", "shadow_wrapperOff");
+                    popUpDiv.removeChild(document.getElementById("iContent"))
+                    console.log("clicked");
+                }else{
+                    console.log("clicking on child parent");
+                }
+            });
         })
     })
     if(searchValue == ""){
@@ -87,13 +157,25 @@ function addLinks(){
         linksContainer.appendChild(a);   
         a.addEventListener("click", (links) =>{
             links.preventDefault();
+            //show overlay if link was clicked
             overlay.setAttribute("id", "shadow_wrapperOn");
+            searchBar.value = "";
+
             document.title = `Lucky | ${i.title}`;
             popupTitle.innerHTML = i.title;
             var newDiv = document.createElement("div");
             newDiv.setAttribute("id", "iContent");
-            newDiv.innerHTML = i.content;
+            newDiv.innerHTML = `<hr><br>` + i.content;
             popUpDiv.appendChild(newDiv)
+            overlay.addEventListener("click", (event)=> {
+                if (event.target === overlay) {
+                    overlay.setAttribute("id", "shadow_wrapperOff");
+                    popUpDiv.removeChild(document.getElementById("iContent"))
+                    console.log("clicked");
+                }else{
+                    console.log("clicking on child parent");
+                }
+            });
         })
     })
 }
@@ -105,4 +187,25 @@ closeButton.addEventListener("click", (e) =>{
     popUpDiv.removeChild(document.getElementById("iContent"))
     searchBar.value = "";
     document.title = "Lucky";
+})
+
+
+document.addEventListener("keydown", function(event){
+    if (event.key === "Escape") {
+        overlay.setAttribute("id", "shadow_wrapperOff");
+        popUpDiv.removeChild(document.getElementById("iContent"))
+        console.log("ESC key was pressed!");
+        suggestions.innerHTML = "";
+        searchBar.blur();
+    }
+    if (event.key === "/"){
+        searchBar.focus();
+    }
+})
+
+
+document.addEventListener("keydown", function(event){
+    if (event.key === ";") {
+        console.log("fuck you");
+    }
 })
